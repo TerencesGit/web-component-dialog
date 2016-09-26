0570ea32f9fde6419528b08145acc43091fd3063
transformGit <2630243397@qq.com>
2016-09-26 11:24:56 +0800

    参数控制

--- js/dialog.js
+++ js/dialog.js
@@ -1,7 +1,6 @@
 ;(function($){
 
 	var Dialog = function(config){
-		var _this = this;
 		//默认参数设置
 		this.config = {
 			width: 'auto',
@@ -22,14 +21,32 @@
 	Dialog.prototype = {
 		renderUI: function(){
 			var _this = this,
-					config = this.config;
+			    config = this.config;
 			this.modal = $('<div class="dialog-modal"></div>');
-			var dialog = $('<div class="dialog-container"><div>')
-			var header = $('<div class="dialog-header"></div>');
+			var dialog = $('<div class="dialog-container"><div>'),
+			    header = $('<div class="dialog-header"></div>'),
+			    body = $('<div class="dialog-body"></div>'),
+			    footer = $('<div class="dialog-footer"></div>');
 			if(this.isConfig){
 					dialog.append(header.addClass('waiting'));
+			}else{
+				this.modal.css({
+					background: 'rgba(0,0,0,'+config.maskOpcity+')'
+				})
+				dialog.width(config.width);
+				dialog.height(config.height);
+				dialog.append(header.addClass(config.type))
+				dialog.append(body.html(config.message))
 			}	
 			this.modal.append(dialog).appendTo($('body'))
+			if(config.delay){
+				window.setTimeout(function(){
+					_this.destroy()
+				},config.delay)
+			}
+		},
+		destroy: function(){
+			this.modal && this.modal.remove();
 		}
 	};
 	window.Dialog = Dialog;
